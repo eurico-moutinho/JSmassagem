b5b8585f3a7df480221a4099b28c29c1b4bb6bf9
Eurico Moutinho <yungsniper@hotmail>
2022-10-27 09:59:56 +0100

    almost

--- src/pages/Osteopatia.jsx
+++ src/pages/Osteopatia.jsx
@@ -4,19 +4,21 @@ function Osteopatia() {
   return (
     <div className="art">
       <h1>Osteopatia</h1>
-      <article>
-        <img src="../osteopathy.jpg" />
-        <p>
-          A osteopatia é a terapêutica que tem como objetivo diagnosticar
-          diferencialmente, tratar e prevenir distúrbios
-          neuro-músculo-esqueléticos e outras alterações relacionadas,
-          utilizando uma variedade de técnicas manuais e outras afins
-          necessárias ao bom desempenho osteopático para melhorar funções
-          fisiológicas e ou a regulação da homeostase que pode estar alterada
-          por disfunções somáticas, neuro-músculo-esqueléticas e elementos
-          vasculares, linfáticos e neuronais relacionados."
-        </p>
-      </article>
+      <div>
+        <article>
+          <img src="../osteopathy.jpg" />
+          <p>
+            A osteopatia é a terapêutica que tem como objetivo diagnosticar
+            diferencialmente, tratar e prevenir distúrbios
+            neuro-músculo-esqueléticos e outras alterações relacionadas,
+            utilizando uma variedade de técnicas manuais e outras afins
+            necessárias ao bom desempenho osteopático para melhorar funções
+            fisiológicas e ou a regulação da homeostase que pode estar alterada
+            por disfunções somáticas, neuro-músculo-esqueléticas e elementos
+            vasculares, linfáticos e neuronais relacionados."
+          </p>
+        </article>
+      </div>
     </div>
   );
 }
