651cbd4d2992d4f5cccbf7160f8f490d96f77071
Eurico Moutinho <yungsniper@hotmail>
2022-10-27 09:50:15 +0100

    almost

--- src/pages/MassagemDesportiva.jsx
+++ src/pages/MassagemDesportiva.jsx
@@ -4,18 +4,20 @@ function MassagemDesportiva() {
   return (
     <div className="art">
       <h1>Massagem Desportiva</h1>
-      <article>
-        <img src="../massagemdesport.jpg" />
-        <p>
-          Os atletas após submeterem-se a um intenso esforço físico, sofrem
-          sintomas de dores musculares, dores nas articulações e inflamações dos
-          tendões. Como tal é aconselhavel a massagem desportiva, pois ajuda na
-          recuperação muscular, alivia a tensão muscular e facilita o processo
-          de remoção das toxinas produzidas pelo corpo como o ácido láctico.
-          Após a massagem, o corpo terá uma redução de fadiga muscular
-          significativa.
-        </p>
-      </article>
+      <div>
+        <article>
+          <img src="../massagemdesport.jpg" />
+          <p>
+            Os atletas após submeterem-se a um intenso esforço físico, sofrem
+            sintomas de dores musculares, dores nas articulações e inflamações
+            dos tendões. Como tal é aconselhavel a massagem desportiva, pois
+            ajuda na recuperação muscular, alivia a tensão muscular e facilita o
+            processo de remoção das toxinas produzidas pelo corpo como o ácido
+            láctico. Após a massagem, o corpo terá uma redução de fadiga
+            muscular significativa.
+          </p>
+        </article>
+      </div>
     </div>
   );
 }
